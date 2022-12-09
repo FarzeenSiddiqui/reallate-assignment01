79aef771a30efd148811b8dde6c10d4e71a2f552
Farid <99864644+FarzeenSiddiqui@users.noreply.github.com>
2022-12-09 12:01:39 +0530

    Final Commit

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Developer</title>
+</head>
+
+<body>
+    <main>
+
+        <section class="section__map">
+            <div class="row m-0 justify-content-center">
+                <div class="col-xl-9 col-lg-10 col-md-10 col-sm-10">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                            <div class="map-content">
+                                <div class="map_div">
+                                    <img src="./assets/images/map.png" alt="map-image" data-aos="fade-up">
+                                    <p data-aos="fade-up">
+                                        Please share your desired location with us so that we can help you with the best possible propertes.
+                                    </p>
+                                </div>
+                                <div class="view-div" data-aos="fade-up">
+                                    <a href="" target="_blank">view map</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                            <div class="map-content pt-md-0 pt-5">
+                                <div class="map_div">
+                                    <img src="./assets/images/post_property.png" alt="map-image" data-aos="fade-up">
+                                    <p data-aos="fade-up">
+                                        Please share your property with us so that we can help you with the best possible solutions.
+                                    </p>
+                                </div>
+                                <div class="view-div" data-aos="fade-up">
+                                    <a href="" target="_blank">post&nbsp;property</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                            <div class="map-content pt-md-0 pt-5">
+                                <div class="map_div">
+                                    <img src="./assets/images/post_requirement.png" alt="map-image" data-aos="fade-up">
+                                    <p data-aos="fade-up">
+                                        Let us have the details of your requirement and we will get back to you immediately!
+                                    </p>
+                                </div>
+                                <div class="view-div" data-aos="fade-up">
+                                    <a href="" target="_blank">post&nbsp;requirement</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section__form">
+            <div class="row m-0 justify-content-center">
+                <div class="col-xl-10 col-lg-11 col-md-11 col-sm-12">
+                    <div class="row g-0">
+                        <div class="col-xl-7 col-ld-7 col-md-7 col-sm-12">
+                            <div class="form-image position-relative">
+                                <img src="./assets/images/project_image.jpg" alt="project-image">
+                                <div class="financial-div" data-aos="zoom-in">
+                                    <h2>12 lacs <br><span>onwards</span></h2>
+                                    <div class="finan mt-md-3">
+                                        <p>Possession<br><span>Dec, 2016</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-5 col-ld-5 col-md-5 col-sm-12">
+                            <div class="form-container">
+                                <h2>Trehan sanskruti Residency</h2>
+                                <p> poonam nagar, andheri east</p>
+                                <div class="form-div pt-5">
+                                    <h2>For more information</h2>
+                                    <input type="text" name="name" placeholder="Enter your name">
+                                    <input type="number" name="mobile" placeholder="Enter your Mobile number" required>
+                                    <button type="submit" value="submit">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section__property">
+            <div class="row m-0 justify-content-center">
+                <div class="col-xl-9 col-lg-10 col-md-10 col-sm-10">
+                    <div class="pro-title position-relative" data-aos="fade-up">
+                        <h2>Hot properties</h2>
+                    </div>
+                    <div class="row pt-5">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="sale-btn" data-aos="fade-up">
+                                <h2>for sale</h2>
+                            </div>
+                            <div class="row pt-5">
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>Dahisar East</h3>
+                                        <p>1BHK Multistorey Apartment<br>
+                                            60 lacs-70 lacs</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>Andheri west</h3>
+                                        <p>2BHK Multistorey Apartment<br>
+                                            1.8 crores-2crores</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>Bhandup West</h3>
+                                        <p>2BHK Multistorey Apartment<br>
+                                            1.2 crores-1.4 crores</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>kandivali west</h3>
+                                        <p>1BHK Multistorey Apartment<br>
+                                            60 lacs-70 lacs</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="srch pt-md-5 pt-3" data-aos="fade-up">
+                                <p>Over 200 properties for sale in Mumbai <span><a href="" target="_blank">Search Now</a></span></p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 pt-md-0 pt-5">
+                            <div class="sale-btn" data-aos="fade-up">
+                                <h2>for rent</h2>
+                            </div>
+                            <div class="row pt-5">
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>Dahisar East</h3>
+                                        <p>1BHK Multistorey Apartment<br>
+                                            60 lacs-70 lacs</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>Andheri west</h3>
+                                        <p>2BHK Multistorey Apartment<br>
+                                            1.8 crores-2crores</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>Bhandup West</h3>
+                                        <p>2BHK Multistorey Apartment<br>
+                                            1.2 crores-1.4 crores</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-4">
+                                    <div class="add-div" data-aos="fade-up">
+                                        <h3>kandivali west</h3>
+                                        <p>1BHK Multistorey Apartment<br>
+                                            60 lacs-70 lacs</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="srch pt-md-5 pt-3" data-aos="fade-up">
+                                <p>Over 200 properties for sale in Mumbai <span><a href="" target="_blank">Search Now</a></span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section__project">
+            <div class="row m-0 justify-content-center">
+                <div class="col-xl-7 col-lg-8 col-md-10 col-sm-10">
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                            <div class="prj-no mb-3" data-aos="fade-zoom-in">
+                                <h2>10+</h2>
+                            </div>
+                            <div class="prj-para" data-aos="fade-up">
+                                <p>new projects</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                            <div class="prj-no mb-3" data-aos="fade-zoom-in">
+                                <h2>120+</h2>
+                            </div>
+                            <div class="prj-para" data-aos="fade-up">
+                                <p>properties for sale</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                            <div class="prj-no mb-3" data-aos="fade-zoom-in">
+                                <h2>50+</h2>
+                            </div>
+                            <div class="prj-para" data-aos="fade-up">
+                                <p>properties for rent</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                            <div class="prj-no mb-3" data-aos="fade-zoom-in">
+                                <h2>100+</h2>
+                            </div>
+                            <div class="prj-para" data-aos="fade-up">
+                                <p>happy customer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section__slider">
+            <div class="row m-0 justify-content-center">
+                <div class="col-xl-8 col-lg-9 col-md-9 col-sm-10">
+                    <div class="swiper mySwiper">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="slide-image">
+                                    <img src="./assets/images/testimonial.png" alt="">
+                                    <p>
+                                        "I m wondering why I never contacted these guys sooner,they all have commendable talent in thei respective field and knocked my concept out of the ballpark.Thanks for an amazing experience!"
+                                    </p>
+                                    <h6 class="py-5">-segero Noranicmeds</h6>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="slide-image">
+                                    <img src="./assets/images/testimonial.png" alt="">
+                                    <p>
+                                        "I m wondering why I never contacted these guys sooner,they all have commendable talent in thei respective field and knocked my concept out of the ballpark.Thanks for an amazing experience!"
+                                    </p>
+                                    <h6 class="py-5">-segero Noranicmeds</h6>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="slide-image">
+                                    <img src="./assets/images/testimonial.png" alt="">
+                                    <p>
+                                        "I m wondering why I never contacted these guys sooner,they all have commendable talent in thei respective field and knocked my concept out of the ballpark.Thanks for an amazing experience!"
+                                    </p>
+                                    <h6 class="py-5">-segero Noranicmeds</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-pagination"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="section__footer">
+        <div class="row m-0 justify-content-center">
+            <div class="col-xl-10 col-lg-10 col-md-10 col-sm-12">
+                <div class="footer-title">
+                    <h2>Developers</h2>
+                </div>
+                <div class="footer-image">
+                    <div class="footer-logo">
+                        <a href="" target="_blank">
+                            <img src="./assets/images/developer1.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="footer-logo">
+                        <a href="" target="_blank">
+                            <img src="./assets/images/developer-2.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="footer-logo">
+                        <a href="" target="_blank">
+                            <img src="./assets/images/developer-3.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="footer-logo">
+                        <a href="" target="_blank">
+                            <img src="./assets/images/developer-4.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+<!--  -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 1200,
+        easing: 'ease-in-cubic',
+    });
+</script>
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+    });
+</script>
+
+</html>
